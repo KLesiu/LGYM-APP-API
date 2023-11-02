f372faec2f8cc948cf0664a81bf09f5c24eb738f
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-02 16:57:21 +0100

    Add getPlanConfig fn

--- controllers/planController.js
+++ controllers/planController.js
@@ -8,10 +8,19 @@ require("dotenv").config()
 exports.setPlanConfig=async(req,res,next)=>{
     const days = +req.body.days
     const name = req.body.name
+    const id = req.params.id
     if(!name || !days) return res.send({msg:'All fields are required'})
     if(days< 1 || days > 7) return res.send({msg:'You have to choose days number between 1-7'})
-    const findUser = await User.findById(req.params.id)
+    const findUser = await User.findById(id)
     const currentPlan = await Plan.create({user:findUser,name:name,trainingDays:days})
     await findUser.updateOne({plan:currentPlan})
     return res.send({msg:"Created"})
+}
+
+exports.getPlanConfig=async(req,res,next)=>{
+    const id = req.params.id
+    const findUser = await User.findById(id)
+    const findPlan = await Plan.findOne({user:findUser})
+    
+    return res.send({count:findPlan.trainingDays})
 }
--- routes/plan.js
+++ routes/plan.js
@@ -2,5 +2,6 @@ const Router = require("express").Router()
 const planController = require("../controllers/planController")
 
 Router.post('/:id/configPlan',planController.setPlanConfig)
+Router.get('/:id/configPlan',planController.getPlanConfig)
 
 module.exports = Router
