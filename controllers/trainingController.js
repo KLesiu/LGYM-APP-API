02f494f985fda69c730df186ccddad2e4878ce82
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-11-07 23:34:17 +0100

    Add getCurrentTrainingSession function that send information about currentTrainingSession

--- controllers/trainingController.js
+++ controllers/trainingController.js
@@ -13,3 +13,23 @@ exports.addTraining=async(req,res,next)=>{
     if(newTraining) res.status(200).send({msg:'Training added'})
     else res.status(404).send({msg:'Error, We didnt add your training!'})
 }
+
+exports.getTrainingHistory=async(req,res,next)=>{
+    const id = req.params.id
+    const findUser = await User.findById(id)
+    if(findUser){
+        const trainings = await Training.find({user:findUser})
+        if(trainings.length>0) return res.status(200).send({trainingHistory:trainings})
+        else return res.status(404).send({msg:'You dont have trainings!'})
+    }
+    else return res.status(404).send({msg:'Error, we dont find You in our database. Please logout and login one more time.'})
+}
+
+exports.getCurrentTrainingSession=async(req,res,next)=>{
+    const id = req.params.id
+    const findTraining = await Training.findById(id)
+    if(findTraining){
+        return res.status(200).send({training:findTraining})
+    }
+    else return res.status(404).send({msg:'We dont find your training session!, Please logout and login one more time'})
+}
--- routes/training.js
+++ routes/training.js
@@ -2,5 +2,7 @@ const Router = require("express").Router()
 const trainingController = require('../controllers/trainingController')
 
 Router.post('/:id/addTraining',trainingController.addTraining)
+Router.get('/:id/getTrainingHistory',trainingController.getTrainingHistory)
+Router.get('/:id/getTrainingSession',trainingController.getCurrentTrainingSession)
 
 module.exports = Router
